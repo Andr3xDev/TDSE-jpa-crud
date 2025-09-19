8a87827f9e531638768b265f35c206ed78f0d5f2
Andr3xDev <andr3xdev@gmail.com>
2025-09-19 05:14:51 +0000

    feature: page view

---
+++ src/main/java/edu/escuelaing/tdse/controller/viewController.java
@@ -0,0 +1,22 @@
+package edu.escuelaing.tdse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller to handle view navigation.
+ */
+@Controller
+public class viewController {
+
+    /**
+     * Maps the "/home" endpoint to return the "inmobiliaria" view.
+     *
+     * @return The name of the view template to be rendered.
+     */
+    @GetMapping("/home")
+    public String home() {
+        return "inventory";
+    }
+
+}
